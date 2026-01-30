5b1df6778522ae7c01867df8d245248fc1c0a402
iOS-GM <1107154510@qq.com>
2026-01-30 15:04:32 +0800

    Auto update_git 2026-01-30 15:04

--- update.sh
+++ update.sh
@@ -6,14 +6,22 @@ cd /var/mobile/ad
 dpkg-scanpackages -m . /dev/null > Packages
 xz -c Packages > Packages.xz
 bzip2 -c Packages > Packages.bz2
+gzip -c Packages > Packages.gz
+zstd -c Packages > Packages.zst
 
 # 计算校验值
+#### pkg
 md5_pkg=$(md5sum Packages | cut -d' ' -f1)
 size_pkg=$(wc -c < Packages)
+#### xz
 md5_xz=$(md5sum Packages.xz | cut -d' ' -f1)
 size_xz=$(wc -c < Packages.xz)
+#### bz2
 md5_bz2=$(md5sum Packages.bz2 | cut -d' ' -f1)
 size_bz2=$(wc -c < Packages.bz2)
+#### zst
+md5_zst=$(md5sum Packages.zst | cut -d' ' -f1)
+size_zst=$(wc -c < Packages.zst)
 
 # 提交更新
 git add .
--- update_dpkg.sh
+++ update_dpkg.sh
@@ -7,5 +7,6 @@ dpkg-scanpackages -m . /dev/null > Packages
 xz -c Packages > Packages.xz
 bzip2 -c Packages > Packages.bz2
 gzip -c Packages > Packages.gz
+zstd -c Packages > Packages.zst
 
 ./update_git.sh
